3a716cd4261c1f7b887a3dffabf55ae11c36d393
Kiryanov <gildorfeataur@gmail.com>
2020-05-26 01:15:35 +0300

    Add buttons to paginator, fix view

--- src/components/Users/UsersList/index.js
+++ src/components/Users/UsersList/index.js
@@ -7,25 +7,58 @@ import styles from "./index.module.scss";
 function UsersList({ data, changeFollowing, changePage }) {
   let totalPages = Math.ceil(data.usersTotalCount / data.itemsPerPage);
   let buttons = [];
-  for (let index = 1; index <= totalPages; index++) {
-    buttons.push(index);
+  let range = 10;
+  const [start, setStart] = React.useState(1);
+  const [buttonsPerPage, setButtonsPerPage] = React.useState(range);
+
+  if (data.currentPage === buttonsPerPage) {
+    setStart(buttonsPerPage);
+    setButtonsPerPage(buttonsPerPage + range);
   }
+
+  for (let index = start; index <= totalPages; index++) {
+    if (index <= buttonsPerPage) {
+      buttons.push(index);
+    }
+    if (index === totalPages - 1) {
+      buttons.push("...");
+      buttons.push(totalPages);
+    }
+  }
+
   return (
     <section className={styles.container}>
       <h2>Users list</h2>
 
       <div className={styles.paginator}>
-        {buttons.map((item) => {
-          return (
-            <button
-              key={item}
-              className={data.currentPage === item ? styles.isActive : null}
-              onClick={() => changePage(item)}
-            >
-              {item}
-            </button>
-          );
-        })}
+        <button
+          className={styles.paginatorBtn}
+          value="prev"
+          onClick={() => changePage(data.currentPage - 1)}
+          disabled={data.currentPage === 1}
+        >
+          Prev
+        </button>
+        <div className={styles.paginatorPages}>
+          {buttons.map((item) => {
+            return (
+              <button
+                key={item}
+                className={data.currentPage === item ? styles.isActive : null}
+                onClick={() => changePage(item)}
+              >
+                {item}
+              </button>
+            );
+          })}
+        </div>
+        <button
+          className={styles.paginatorBtn}
+          value="next"
+          onClick={() => changePage(data.currentPage + 1)}
+        >
+          Next
+        </button>
       </div>
 
       {data.isFetching ? (
