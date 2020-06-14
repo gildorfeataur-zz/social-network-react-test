fccf33c28acc1124c6c36247065071350c81b464
Kiryanov <gildorfeataur@gmail.com>
2020-06-14 14:13:41 +0300

    Updates

---
+++ precache-manifest.20a40ebca6454e283db97ad2feb1d781.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8aebdecca9fc8cd84ee996530419f7",
+    "url": "/social-network-react-test/index.html"
+  },
+  {
+    "revision": "bc6c29db0462bceb38c6",
+    "url": "/social-network-react-test/static/css/3.3f2e1e8e.chunk.css"
+  },
+  {
+    "revision": "a5839d901bd9129d8f88",
+    "url": "/social-network-react-test/static/css/main.bf1e2951.chunk.css"
+  },
+  {
+    "revision": "6879e3249a138444ccf8",
+    "url": "/social-network-react-test/static/js/2.e5434463.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network-react-test/static/js/2.e5434463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc6c29db0462bceb38c6",
+    "url": "/social-network-react-test/static/js/3.0d5d720c.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/social-network-react-test/static/js/3.0d5d720c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5839d901bd9129d8f88",
+    "url": "/social-network-react-test/static/js/main.68d84d78.chunk.js"
+  },
+  {
+    "revision": "88dde68aced4a43e98c7",
+    "url": "/social-network-react-test/static/js/runtime-main.5c0811d1.js"
+  },
+  {
+    "revision": "32e9686bd991e1c7ee89e16108ba0666",
+    "url": "/social-network-react-test/static/media/avatar-default.32e9686b.png"
+  }
+]);
